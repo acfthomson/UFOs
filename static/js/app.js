fd36e0efe73c48023297badf878979eb6fa352ea
Amanda Thomson <acfthomson@gmail.com>
2021-02-15 14:56:33 -0500

    added handleClick function

--- static/js/app.js
+++ static/js/app.js
@@ -33,3 +33,34 @@ function buildTable(data) {
         );
     });
 }
+
+// Filter a table
+function handleClick() {
+
+    // Use D3.js library to equip website to listen for events, such as button clicks
+    // Use .select() to select the first element that matches selector string "#datetime"
+    // Tell D3 to look for date values stored on the webpage and grab that information and hold
+    // it in the date variabe
+    let date = d3.select("#datetime").property("value");
+
+    // Set a default filter and save it to a new variable
+    let filteredData = tableData;
+
+    // Check for a date
+    if (date) {
+
+        // Apply thr filter to the table data
+        // === checks for strict equality
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    // Rebuild the tabke using the filtered data
+    // If a date was not entered, the filteredData will be the original tableData
+    buildTable(filteredData);
+}
+
+// Listen for a filter button click
+d3.selectAll(#filter-btn).on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
